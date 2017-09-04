8cf9a2c0c90154229764df9a4796e21a0987072e
Liam Davies <lmdudester@gmail.com>
2017-09-03 20:58:57 -0400

    Added bus tags finder to agency and name changed

---
+++ findBusTags.py
@@ -0,0 +1,20 @@
+import json, requests
+
+nbBusses = "A B C EE F H LX REX L REX B Weekend 1 Weekend 2"
+
+#Request Agency Listing
+agencyResp = requests.get("http://webservices.nextbus.com/service/publicJSONFeed?command=agencyList")
+agencyResp.raise_for_status()
+agencyList = json.loads(agencyResp.text)
+
+#Obtain tag for Rutgers
+for i in agencyList["agency"]:
+    if "Rutgers University" == i["title"]:
+        rutgersDict = i
+
+#Get Route List
+routeResp = requests.get("http://webservices.nextbus.com/service/publicJSONFeed?command=routeList&a=" + rutgersDict["tag"])
+routeResp.raise_for_status()
+busDict = json.loads(routeResp.text)
+
+#busDict now has all bus routes and their tags inside

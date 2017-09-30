94307f7a3b06a306ee40f1f8a0476b8f9e43424d
Liam Davies <lmdudester@gmail.com>
2017-09-30 15:57:35 -0400

    Implemented greedy algo to better handle lots of requests

--- requestChecker.py
+++ requestChecker.py
@@ -59,42 +59,68 @@ def sendEmail(conn, userId):
     server.sendmail(fromaddr, toaddrs, msg)
     server.quit()
 
-    print("Name: %s" % usrInfo[0])
-    print("Email: %s" % usrInfo[1])
-    print("BusName: %s" % usrInfo[2])
-    print("StopName: %s" % usrInfo[3])
+#Obtains and processes all relevant requests
+def getRequests(conn, currTime, timeBuffer):
+    currTime.addMins(timeBuffer)
+
+    #Get every request within the threshold
+    c = conn.execute("SELECT id, departHour, departMinute, timeBuffer, stopTag, busTag FROM Requests WHERE (departHour = " + str(currTime.hour) + " AND departMinute <= " + str(currTime.minute) + ") OR departHour < " + str(currTime.hour) + ";")
+
+    #Bring time back to current time
+    currTime.addMins(-1*timeBuffer)
+
+    return c
+
+def createGreedyDict(conn, currTime):
+    preDict = {}
+    busTags = conn.execute("SELECT busTag FROM StopsBusses GROUP BY busTag")
+
+    for btag in busTags:
+        preDict[btag[0]] = {}
+
+        stopTags = conn.execute("SELECT stopTag FROM StopsBusses WHERE busTag = \'" + btag[0] + "\' GROUP BY stopTag;")
+
+        for stag in stopTags:
+            preDict[btag[0]][stag[0]] = []
+
+            predictions = requests.get("http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=rutgers&r=" + btag[0] + "&s=" + stag[0])
+            predictions.raise_for_status()
+            preList = json.loads(predictions.text)["predictions"]['direction']['prediction']
+
+            for timeDict in preList:
+                temp = TimeKeeper(currTime.hour, currTime.minute)
+                temp.addMins(int(timeDict['minutes']))
+                preDict[btag[0]][stag[0]] += [ temp ]
+
+    return preDict
 
 #Determines if an alert is required for the given request
-def determineRequest(conn, currTime, request):
-    predictions = requests.get("http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=rutgers&r=" + request[5] + "&s=" + request[4])
-    predictions.raise_for_status()
-    preList = json.loads(predictions.text)["predictions"]['direction']['prediction']
+def processRequest(conn, preDict, currTime, request):
+    reqPreList = preDict[request[5]][request[4]]
 
     #Creates a TimeKeeper object for the required leaving time
     reqTime = TimeKeeper(request[1], request[2])
 
     #Soonest prediciton is defaulted to best time
-    bestTime = TimeKeeper(currTime.hour, currTime.minute)
-    bestTime.addMins(int(preList[0]['minutes']))
+    suggTime = reqPreList[0]
 
     #While the current time plus the prediction is still before the required time
     #   keep trying to find the latest time
-    for preDict in preList[1:]:
-        tempTime = TimeKeeper(currTime.hour, currTime.minute)
-        tempTime.addMins(int(preDict['minutes']))
-
-        if(tempTime.__cmp__(reqTime) > 0):
+    for preDict in reqPreList[1:]:
+        if(preDict.__cmp__(reqTime) > 0):
             break
         else:
-            bestTime = tempTime
+            suggTime = preDict
+
+    arriveTime = TimeKeeper(suggTime.hour, suggTime.minute)
 
     #Account for the walking time buffer
-    bestTime.addMins(-1*request[3])
+    arriveTime.addMins(-1*request[3])
 
     #If its time to leave or the time to leave has passed alert the user
-    if(currTime.__cmp__(bestTime) >= 0):
-        bestTime.addMins(request[3])
-        print("Leave now id: %d Catch: %s\n" % (request[0], str(bestTime)))
+    if(currTime.__cmp__(arriveTime) >= 0):
+        arriveTime.addMins(request[3])
+        print("Leave Now\tID: %d - Catch: %s\n" % (request[0], str(arriveTime)))
 
         #Send email
         sendEmail(conn, request[0])
@@ -105,26 +131,8 @@ def determineRequest(conn, currTime, request):
 
     #Otherwise, wait for next refresh
     else:
-        bestTime.addMins(request[3])
-        print("Request id: %d Best prediction: %s\n" % (request[0], str(bestTime)))
-
-#Obtains and processes all relevant requests
-def getRequests(conn):
-    #Create a TimeKeeper object of the current time
-    now = datetime.datetime.now()
-    time = TimeKeeper(now.hour, now.minute)
-    time.addMins(45)
-
-    #Get every request within the threshold
-    c = conn.execute("SELECT id, departHour, departMinute, timeBuffer, stopTag, busTag FROM Requests WHERE (departHour = " + str(time.hour) + " AND departMinute <= " + str(time.minute) + ") OR departHour < " + str(time.hour) + ";")
-
-    #Bring time back to current time
-    time.addMins(-45)
-    print("Time: %s\n" % str(time))
-
-    #Process each request
-    for row in c:
-        determineRequest(conn, time, row)
+        arriveTime.addMins(request[3])
+        print("Request ID: %d - Best prediction: %s\n" % (request[0], str(arriveTime)))
 
 def main():
     #Connect to the database
@@ -132,10 +140,20 @@ def main():
 
     #Process requests every minute
     while(True):
-        getRequests(conn)
+        #Create a TimeKeeper object of the current time
+        now = datetime.datetime.now()
+        currTime = TimeKeeper(now.hour, now.minute)
+        print("Log Time: %s\n" % str(currTime))
+
+        requests = getRequests(conn, currTime, 45)
+
+        preDict = createGreedyDict(conn, currTime)
+
+        for request in requests:
+           processRequest(conn, preDict, currTime, request)
+
         print("Success.")
         time.sleep(60)
-
 
     print("Success.")
 

0d3780e02b414cfd39175250d23ef8342b66655d
Liam Davies <lmdudester@gmail.com>
2017-10-01 19:46:01 -0400

    Documented addRequest.py

--- addRequest.py
+++ addRequest.py
@@ -1,5 +1,13 @@
 import sqlite3
 
+# **getInput()**
+# Loops question to get a valid response
+# Args:
+#   reqStr - the question or prompt for the user
+#   failStr - what to print if the input is invalid
+#   choiceList - a list of valid strings the user can type
+# Return:
+#   The valid response string the user typed
 def getInput(reqStr, failStr, choiceList):
     resp = input(reqStr).lower()
 
@@ -14,9 +22,10 @@ def main():
     #Connect to or create the database file
     conn = sqlite3.connect('databases/busSniper.db')
 
+    #Start looping
     resp = 'n'
-
     while resp == 'n':
+        #Get Request Info
         name = input("Name: ")
         print()
         email = input("Email: ")
@@ -27,6 +36,7 @@ def main():
         print()
         timeBuff = int(getInput("Depart Buffer: (0-15) ", "Not a valid depart buffer.\n", [str(x) for x in range(0, 15)]))
 
+        #Pull bus names and only allow valid bus names
         busInfo = conn.execute("SELECT busName, busTag FROM StopsBusses GROUP BY busTag;")
         busTagList = []
 
@@ -37,6 +47,7 @@ def main():
         print()
         bus = getInput("Bus Tag: ", "Not a valid bus tag.\n", busTagList)
 
+        #Pull stop names for the selected bus and only allow valid stop names
         stopInfo = conn.execute("SELECT stopName, stopTag FROM StopsBusses WHERE busTag = \'" + bus + "\' GROUP BY stopTag;")
         stopTagList = []
 
@@ -47,6 +58,7 @@ def main():
         print()
         stop = getInput("Stop Tag: ", "Not a valid stop tag.\n", stopTagList)
 
+        #Give the user a chance to review the data they want to enter
         print("\n----Review Info----")
         print("Name: %s" % name)
         print("Email: %s" % email)
@@ -57,6 +69,7 @@ def main():
 
         resp = getInput("Is this info correct? (Y/N)", "Not a valid response.", ['y', 'n'])
 
+    #Enter the data into the database
     conn.execute("INSERT INTO Requests (name, email, departHour, departMinute, timeBuffer, busTag, stopTag) VALUES (\'" +
                     name + "\', \'" + email + "\', " + str(hour) + ", " + str(minute) + ", " + str(timeBuff) + ", \'" + bus + "\', \'" +
                     stop + "\');")
